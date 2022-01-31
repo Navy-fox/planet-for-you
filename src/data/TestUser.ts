7cd0015ac0de01a41305a2a9de93fa8fd89a3584
navyFox <lina.666@list.ru>
2022-02-01 00:25:04 +0300

    FRONT: Добавила тип пользователя и тестового пользователя

---
+++ src/data/TestUser.ts
@@ -0,0 +1,11 @@
+import { User } from '@/types/User'
+
+export const TEST_USERS: User[] = [
+  {
+    id: 8,
+    email: 'lindsay.ferguson@reqres.in',
+    first_name: 'Lindsay',
+    last_name: 'Ferguson',
+    avatar: 'https://reqres.in/img/faces/8-image.jpg',
+  },
+]
